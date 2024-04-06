7b3f7e0aa58bc2cc6c2c4f9f754dc662f667e6bb
Abdulla-Bin-Islam <21-45122-2@student.aiub.edu>
2024-04-06 22:07:35 +0600

    added in orm config

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,6 +1,9 @@
 import { Applyinstructor } from 'src/entities/applyinstructor.entity';
+import { Blog } from 'src/entities/blog.entity';
 import { Category } from 'src/entities/category.entity';
+import { ContractForm } from 'src/entities/contract-form.entity';
 import { Course } from 'src/entities/course.entity';
+import { Invoice } from 'src/entities/invoice.entity';
 import { Order } from 'src/entities/order.entity';
 import { User } from 'src/entities/user.entity';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
@@ -13,7 +16,7 @@ const config: PostgresConnectionOptions = {
   username: 'postgres',
 
   password: 'admin',
-  entities: [Course, Category, User, Order,Applyinstructor],
+  entities: [Course, Category, User, Order,Applyinstructor,Invoice,Blog,ContractForm],
 
   
   synchronize: true,
