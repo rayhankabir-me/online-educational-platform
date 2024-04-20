75e3ca382d45f65b0b96c6d6baa0d77b6dd63f22
rayhankabir-me <devrayhankabir@gmail.com>
2024-04-20 17:23:03 +0600

    my local changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import config from 'ormconfig';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ApplyinstructorModule } from './applyinstructor/applyinstructor.module';
 import { AuthModule } from './auth/auth.module';
 import { CategoriesModule } from './categories/categories.module';
 import { CoursesModule } from './courses/courses.module';
-import { OrdersModule } from './orders/orders.module';
 import { OrderItemModule } from './order-item/order-item.module';
-import { ApplyinstructorModule } from './applyinstructor/applyinstructor.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
     CoursesModule,
     TypeOrmModule.forRoot(config),
     CategoriesModule,
